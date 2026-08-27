e2445a5a1eab7b33d3ccca875d3dbcfe07f0cea5
webcartaonline <webcartaonline@gmail.com>
2026-08-27 10:41:16 +0200

    segundo intento de actualización

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    entera de que hay algo nuevo.
    ========================================================= */
 
-const VERSION = '1.0.0';
+const VERSION = '1.2.1';
 const CACHE   = `editor-carta-${VERSION}`;
 
 /* Todo lo que hace falta para que la aplicación funcione sin internet.
