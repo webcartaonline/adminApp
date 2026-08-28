6d4c0216efbdd428ed373edeb5723f177071b962
webcartaonline <webcartaonline@gmail.com>
2026-08-28 16:59:52 +0200

    nueva: URL

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
    automáticamente la ventana con lo que ha cambiado.
    ========================================================= */
 
-const VERSION = '1.3';
+const VERSION = '2.0';
 const CACHE   = `editor-carta-${VERSION}`;
 
 /* Todo lo que hace falta para que la aplicación funcione sin internet.
